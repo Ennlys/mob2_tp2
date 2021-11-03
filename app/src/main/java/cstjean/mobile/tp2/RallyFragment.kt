4ecf55722ba4f8d56c6ea0bcc4b960d8e0740b5b
JD <JD@JD-PC>
2021-11-03 15:12:47 -0400

    ajout de couleur pour les markeurs

--- app/src/main/java/cstjean/mobile/tp2/RallyFragment.kt
+++ app/src/main/java/cstjean/mobile/tp2/RallyFragment.kt
@@ -14,6 +14,7 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -105,14 +106,20 @@ class RallyFragment : Fragment(), OnMapReadyCallback {
         )
 
         for (position in mutableList) {
+            val latLng = LatLng(position.latitude, position.longitude)
             if (position.visite) {
-                // TODO
+                googleMap.addMarker(
+                    MarkerOptions().position(latLng)
+                        .title("visité")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                )
             }
             else {
-                val latLng = LatLng(position.latitude, position.longitude)
+
                 googleMap.addMarker(
                     MarkerOptions().position(latLng)
                         .title("non visite")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
                 )
 
                 googleMap.addCircle(
